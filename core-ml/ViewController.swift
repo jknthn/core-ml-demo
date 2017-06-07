c60c19c3cb48e5ed2b4326a928b3d8616ddcd9ff
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2017-06-07 13:14:59 +0200

    Initial Commit

---
+++ core-ml/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  core-ml
+//
+//  Created by jeremi on 07/06/2017.
+//  Copyright © 2017 jk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
